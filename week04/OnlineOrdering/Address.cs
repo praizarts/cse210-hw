36634d493416392918a6077a47337efc49b00d4b
Ignatia Praise <mbachuignatia@gmail.com>
2026-04-01 00:30:34 +0100

    week04 project: onlineordering

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,27 @@
+using System;
+
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _province;
+    private string _country;
+
+    public Address(string street, string city, string province, string country)
+    {
+        _street = street;
+        _city = city;
+        _province = province;
+        _country = country;
+    }
+
+    public bool IsInUSA()
+    {
+        return _country.ToLower() == "usa";
+    }
+
+    public string GetFullAddress()
+    {
+        return $"{_street}\n{_city}, {_province}\n{_country}";
+    }
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+         Console.WriteLine();
+    
+        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
+        Customer customer1 = new Customer("John Doe", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Laptop", "P1001", 800, 1));
+        order1.AddProduct(new Product("Mouse", "P1002", 20, 2));
+
+        Address address2 = new Address("45 Lagos Road", "Lagos", "Lagos", "Nigeria");
+        Customer customer2 = new Customer("Ignatia Mbachu", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Phone", "P2001", 500, 1));
+        order2.AddProduct(new Product("Headset", "P2002", 50, 2));
+        order2.AddProduct(new Product("Charger", "P2003", 25, 1));
+        Console.WriteLine();
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.GetTotalCost()}");
+        Console.WriteLine();
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost()}");
     }
 }
