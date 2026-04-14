3b574fe99402f86fbf10a6df6d1ccea6ffd2cc46
Ignatia Praise <mbachuignatia@gmail.com>
2026-04-14 09:27:36 +0100

    Week07: exerciseTracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine();
+
+         List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0));
+        activities.Add(new Cycling(new DateTime(2022, 11, 3), 45, 12.0));
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 40, 20));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
